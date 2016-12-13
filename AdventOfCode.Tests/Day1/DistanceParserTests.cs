342a0aeff7ef29fb0c7f0ae64bbb04907461455a
Karl Speer <cidthecoatrack@gmail.com>
2016-12-13 17:40:22 -0500

    Day 1

---
+++ AdventOfCode.Tests/Day1/DistanceParserTests.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode.Day1;
+using NUnit.Framework;
+using System;
+using System.Drawing;
+
+namespace AdventOfCode.Tests.Day1
+{
+    [TestFixture]
+    public class DistanceParserTests
+    {
+        private DistanceParser distanceParser;
+
+        [SetUp]
+        public void Setup()
+        {
+            distanceParser = new DistanceParser();
+        }
+
+        [TestCase("R2, L3", 5)]
+        [TestCase("R2, R2, R2", 2)]
+        [TestCase("R5, L5, R5, R3", 12)]
+        public void VerifyFinalCoordinate(string input, int totalBlocks)
+        {
+            var coordinate = distanceParser.GetFinalCoordinate(input);
+            var distance = GetTotalBlocks(coordinate);
+
+            Assert.That(distance, Is.EqualTo(totalBlocks));
+        }
+
+        private int GetTotalBlocks(Point point)
+        {
+            return Math.Abs(point.X) + Math.Abs(point.Y);
+        }
+
+        [Test]
+        public void GetFinalCoordinates()
+        {
+            var input = "L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, L4, L4, L3, L4, L3, L5, R2, R4, L2";
+
+            var coordinate = distanceParser.GetFinalCoordinate(input);
+            var distance = GetTotalBlocks(coordinate);
+
+            Assert.Pass($"Distance is {distance} blocks");
+        }
+
+        [TestCase("R8, R4, R4, R8", 4)]
+        public void VerifyDistanceToSecondVisit(string input, int totalBlocks)
+        {
+            var coordinate = distanceParser.GetFirstPlaceVisitedTwice(input);
+            var distance = GetTotalBlocks(coordinate);
+            Assert.That(distance, Is.EqualTo(totalBlocks));
+        }
+
+        [Test]
+        public void VerifyDistanceToSecondVisit()
+        {
+            var input = "L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, L4, L4, L3, L4, L3, L5, R2, R4, L2";
+
+            var coordinate = distanceParser.GetFirstPlaceVisitedTwice(input);
+            var distance = GetTotalBlocks(coordinate);
+
+            Assert.Pass($"Distance is {distance} blocks");
+        }
+    }
+}
---
+++ AdventOfCode/Day1/DistanceParser.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace AdventOfCode.Day1
+{
+    public class DistanceParser
+    {
+        private enum DIRECTION { NORTH, EAST, SOUTH, WEST };
+
+        public Point GetFinalCoordinate(string input)
+        {
+            var point = new Point(0, 0);
+            var inputs = input.Split(',').Select(d => d.Trim());
+            var direction = DIRECTION.NORTH;
+
+            foreach (var instruction in inputs)
+            {
+                var turn = instruction[0];
+                var rawDistance = instruction.Substring(1, instruction.Length - 1);
+                var distance = Convert.ToInt32(rawDistance);
+
+                if (turn == 'R')
+                    direction = TurnRight(direction);
+                else if (turn == 'L')
+                    direction = TurnLeft(direction);
+
+                point = GoDistance(point, direction, distance);
+            }
+
+            return point;
+        }
+
+        public Point GetFirstPlaceVisitedTwice(string input)
+        {
+            var point = new Point(0, 0);
+            var inputs = input.Split(',').Select(d => d.Trim());
+            var direction = DIRECTION.NORTH;
+
+            var visited = new List<Point>();
+            visited.Add(point);
+
+            foreach (var instruction in inputs)
+            {
+                var turn = instruction[0];
+                var rawDistance = instruction.Substring(1, instruction.Length - 1);
+                var distance = Convert.ToInt32(rawDistance);
+
+                if (turn == 'R')
+                    direction = TurnRight(direction);
+                else if (turn == 'L')
+                    direction = TurnLeft(direction);
+
+                while (distance-- > 0)
+                {
+                    point = Go(point, direction);
+
+                    if (visited.Any(p => p.X == point.X && p.Y == point.Y))
+                        return point;
+                    else
+                        visited.Add(point);
+                }
+            }
+
+            throw new Exception("You done messed up!");
+        }
+
+        private Point Go(Point point, DIRECTION direction)
+        {
+            switch (direction)
+            {
+                case DIRECTION.NORTH: return new Point(point.X, point.Y + 1);
+                case DIRECTION.EAST: return new Point(point.X + 1, point.Y);
+                case DIRECTION.SOUTH: return new Point(point.X, point.Y - 1);
+                case DIRECTION.WEST: return new Point(point.X - 1, point.Y);
+            }
+
+            throw new Exception("You done messed up!");
+        }
+
+        private Point GoDistance(Point point, DIRECTION direction, int distance)
+        {
+            while (distance-- > 0)
+                point = Go(point, direction);
+
+            return point;
+        }
+
+        private DIRECTION TurnLeft(DIRECTION direction)
+        {
+            var numericDirection = Convert.ToInt32(direction);
+            numericDirection = (numericDirection + 3) % 4;
+
+            return (DIRECTION)numericDirection;
+        }
+
+        private DIRECTION TurnRight(DIRECTION direction)
+        {
+            var numericDirection = Convert.ToInt32(direction);
+            numericDirection = (numericDirection + 1) % 4;
+
+            return (DIRECTION)numericDirection;
+        }
+    }
+}
